bc5ab71d6620f3094c00364f176b5abe163af34f
Neuroplastic1 <shafqat.umair@gmail.com>
2022-07-31 16:48:53 -0400

    Added Bounded Queue limitation (maxSize) along with Helper methods hasRoom() and isEmpty()

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,11 +1,20 @@
 const LinkedList = require('./Singly-LinkedList');
 
 class Queue {
-  constructor() {
+  constructor(maxSize = Infinity) {
     this.queue = new LinkedList();
     this.size = 0;
+    this.maxSize = maxSize;
   }
 
+  hasRoom() {
+    return this.size < this.maxSize
+  }
+
+  isEmpty() {
+    return this.size === 0
+  }
+  
   enqueue(data) {
     this.queue.addToTail(data)
     this.size = this.size + 1
