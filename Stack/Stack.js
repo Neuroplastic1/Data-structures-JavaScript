331117034360559303f5e3e6bd6d2ecf01230a2d
Neuroplastic1 <shafqat.umair@gmail.com>
2022-07-31 22:20:35 -0400

    Tested stack with pizza delivery simulation

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -3,26 +3,39 @@ const LinkedList = require("./LinkedList");
 class Stack {
   constructor(maxSize = Infinity) {
     this.stack = new LinkedList();
-    this.size = 0;
     this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  isEmpty() {
+    return this.size === 0;
   }
 
   push(value) {
-    this.stack.addToHead(value);
+    if (this.hasRoom()) {
+      this.stack.addToHead(value);
+      this.size++;
+    } else {
+      throw new error("Stack is full");
+    }
   }
 
   pop() {
-    if (this.size > 0) {
+    if (this.isEmpty()) {
       const value = this.stack.removeHead();
       this.size--;
       return value;
     } else {
-      console.log("Stack is empty.");
+      console.log("Stack is empty!");
     }
   }
 
   peek() {
-    if (this.size > 0) {
+    if (this.isEmpty()) {
       return this.stack.head.data;
     } else {
       return null;
