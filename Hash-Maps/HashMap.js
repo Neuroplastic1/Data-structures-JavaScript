b3d18d1d0171a673f8f0da39d80bdfa902d45a02
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-02 12:43:27 -0400

    Retrieve method -> input: key output: value Uses hash method to find the arrayIndex where the value is stored

--- Hash-Maps/HashMap.js
+++ Hash-Maps/HashMap.js
@@ -2,7 +2,7 @@ class HashMap {
   constructor(size = 0) {
     this.hashmap = new Array(size).fill(null);
   }
-
+ // hash method -> input: key output: arrayIndex
   hash(key) {
     let hashCode = 0;
     for (let i = 0; i < key.length; i++) {
@@ -18,6 +18,12 @@ class HashMap {
     const arrayIndex = this.hash(key)
     // Assign the value to the element at the index derived from hashing, arrayIndex
     this.hashmap[arrayIndex] = value
+  }
+
+  // retrieve method -> input: key output: value
+  retrieve(key) {
+    const arrayIndex = this.hash(key);
+    return this.hashmap[arrayIndex]
   }
 
 }
