4966f24b45f13124b9e09a0c335360f69a1bcedd
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-05 15:23:42 -0400

    Adding a child to TreeNode Each child in children array has to be an instance of a TreeNode, however we allowed our user interface to accept adding data in other forms as well by additionally creating a node for that data.

--- Non-Linear-Data-Structures/Trees/TreeNode.js
+++ Non-Linear-Data-Structures/Trees/TreeNode.js
@@ -3,7 +3,15 @@ class TreeNode {
     this.data = data
     this.children = []
   }
+
+  addChild(child) {
+    if (child instanceof TreeNode) {
+      this.children.push(child);
+    } else {
+      this.children.push(new TreeNode(child));
+    }
 };
+
 
 module.exports = TreeNode;
 
