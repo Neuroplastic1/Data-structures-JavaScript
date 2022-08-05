6b1ed8ae2a8b4ba4cfb716ebc4f3a7cbb84270ad
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-05 15:50:49 -0400

    removeChild() from tree

--- Non-Linear-Data-Structures/Trees/Test-TreeNode-Methods.js
+++ Non-Linear-Data-Structures/Trees/Test-TreeNode-Methods.js
@@ -1,8 +1,23 @@
 const TreeNode = require('./TreeNode');
 const tree = new TreeNode(1);
 
-console.log(tree);
 tree.addChild(15);
+const node = new TreeNode(30);
+tree.addChild(node);
 console.log(tree);
-tree.addChild(new TreeNode(30));
+
+tree.removeChild(15);
 console.log(tree);
+
+tree.removeChild(node);
+console.log(tree);
+/* logs:
+TreeNode {
+  data: 1,
+  children:
+   [ TreeNode { data: 15, children: [] },
+     TreeNode { data: 30, children: [] } ] }
+TreeNode { data: 1, children: [ TreeNode { data: 30, children: [] } ] }
+TreeNode { data: 1, children: [] }
+
+*/
--- Non-Linear-Data-Structures/Trees/TreeNode.js
+++ Non-Linear-Data-Structures/Trees/TreeNode.js
@@ -9,15 +9,38 @@ class TreeNode {
       this.children.push(child);
     } else {
       this.children.push(new TreeNode(child));
+  }
+
+  // remove by node or data
+  removeChild(childToRemove) {
+    // remember the length for recursive case
+    const length = this.children.length;
+    // filter out targetChild (if found) and update the children array
+    this.children = this.children.filter(child => {
+      // If target child is an instance of TreeNode
+      if (childToRemove instanceof TreeNode) {
+        // Compare target child with each child in the children array: basecase 1
+        return childToRemove !== child;
+      } else {
+        // Compare target child with each child's data in the children array: basecase 2
+        return child.data !== childToRemove;
+      }
+    });
+    // If target child is not found in the children array (length remaind the same)
+    // Recursively call .removeChild() for each grandchild.
+    if (length === this.children.length) {
+      this.children.forEach(child => child.removeChild(childToRemove));
     }
+  }
+
 };
 
 
-module.exports = TreeNode;
+  module.exports = TreeNode;
 
-/*
-A tree is composed of tree nodes.
-A tree node is a very simple data structure that contains:
-1) Data
-2) A list of children, where each child is itself a tree node
-*/
+  /*
+  A tree is composed of tree nodes.
+  A tree node is a very simple data structure that contains:
+  1) Data
+  2) A list of children, where each child is itself a tree node
+  */
