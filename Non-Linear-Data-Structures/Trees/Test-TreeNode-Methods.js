79809a2a9e3dc4b750ca90a65b76958bec0d3e8f
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-05 16:41:07 -0400

    Added print() method for TreeNode: Visual representation: '--' -> each level from root node

--- Non-Linear-Data-Structures/Trees/Test-TreeNode-Methods.js
+++ Non-Linear-Data-Structures/Trees/Test-TreeNode-Methods.js
@@ -1,23 +1,61 @@
 const TreeNode = require('./TreeNode');
 const tree = new TreeNode(1);
+const randomize = () => Math.floor(Math.random() * 20);
 
-tree.addChild(15);
-const node = new TreeNode(30);
-tree.addChild(node);
-console.log(tree);
+// add first-level children
+for (let i = 0; i < 3; i++) {
+  tree.addChild(randomize());
+}
 
-tree.removeChild(15);
-console.log(tree);
+// add second-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    tree.children[i].addChild(randomize());
+  }
+}
 
-tree.removeChild(node);
-console.log(tree);
+// add third-level children
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 2; j++) {
+    for (let k = 0; k < 2; k++) {
+      tree.children[i].children[j].addChild(randomize());
+    }
+  }
+}
+
+// pretty-print the tree
+tree.print();
+console.log(tree)
+
 /* logs:
+1
+-- 19
+-- -- 6
+-- -- -- 7
+-- -- -- 5
+-- -- 19
+-- -- -- 2
+-- -- -- 15
+-- 5
+-- -- 5
+-- -- -- 9
+-- -- -- 7
+-- -- 16
+-- -- -- 14
+-- -- -- 3
+-- 5
+-- -- 1
+-- -- -- 7
+-- -- -- 13
+-- -- 14
+-- -- -- 10
+-- -- -- 3
+
 TreeNode {
   data: 1,
   children:
-   [ TreeNode { data: 15, children: [] },
-     TreeNode { data: 30, children: [] } ] }
-TreeNode { data: 1, children: [ TreeNode { data: 30, children: [] } ] }
-TreeNode { data: 1, children: [] }
+   [ TreeNode { data: 19, children: [Object] },
+     TreeNode { data: 5, children: [Object] },
+     TreeNode { data: 5, children: [Object] } ] }
 
-*/
+ */
--- Non-Linear-Data-Structures/Trees/TreeNode.js
+++ Non-Linear-Data-Structures/Trees/TreeNode.js
@@ -9,6 +9,7 @@ class TreeNode {
       this.children.push(child);
     } else {
       this.children.push(new TreeNode(child));
+    }
   }
 
   // remove by node or data
@@ -33,14 +34,23 @@ class TreeNode {
     }
   }
 
+  print(level = 0) {
+    let result = '';
+    for (let i = 0; i < level; i++) {
+      result += '-- ';
+    }
+    console.log(`${result}${this.data}`);
+    this.children.forEach(child => child.print(level + 1));
+  }
+
 };
 
 
-  module.exports = TreeNode;
+module.exports = TreeNode;
 
-  /*
-  A tree is composed of tree nodes.
-  A tree node is a very simple data structure that contains:
-  1) Data
-  2) A list of children, where each child is itself a tree node
-  */
+/*
+A tree is composed of tree nodes.
+A tree node is a very simple data structure that contains:
+1) Data
+2) A list of children, where each child is itself a tree node
+*/
