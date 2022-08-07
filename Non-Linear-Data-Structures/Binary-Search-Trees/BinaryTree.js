64001e0759839efa2985fbf3b4c4dee2e148f5fd
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-07 17:36:26 -0400

    Binary Tree: introduction Specialized tree data structure that is made up of root node, and at most two child branches or subtrees. Each child node is itself a binary tree
    Each node has the following properties:
    
    > data
    > a depth value, where depth of 1 indicates the top level
      of the tree and a depth greater than 1 is
      a level somewhere lower in the tree
    > a left pointer that points to a left child which is
      itself a binary tree, and must have a data lesser
      than the root node’s data
    > a right pointer that points to a right child
      which is itself a binary tree, and must have
      a data greater than the root node’s data

---
+++ Non-Linear-Data-Structures/Binary-Search-Trees/BinaryTree.js
@@ -0,0 +1,10 @@
+class BinaryTree {
+  constructor(value, depth = 1) {
+    this.value = value
+    this.depth = depth
+    this.left= null
+    this.right = null
+  }
+};
+
+module.exports = BinaryTree;
