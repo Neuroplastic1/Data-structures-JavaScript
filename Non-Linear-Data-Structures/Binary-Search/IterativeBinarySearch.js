9cd3734423824a0e44b7b9179521825bb6a973ef
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-07 17:15:16 -0400

    Iterative binary search function: step 2 Iterative checking implement an iterative binary search solution. The function returns the index of the target value from a sorted list. If the target value is not found, the function returns null.
    used the following steps:
    >Initialize the left and right indices as 0 and the length of the array.
    >Create a while loop that continues to execute until the
     left index equals the right index.
    >Get the value at the index that falls in the middle of left and right.
    >Return the index if the value is equal to target.
    >Set left equal to the current index plus one if the target is
     greater than the value.
    >Set right equal to the current index if the target is
    less than the value.
    
    note: it’s important to remember that the
    function will only work on sorted lists

--- Non-Linear-Data-Structures/Binary-Search/IterativeBinarySearch.js
+++ Non-Linear-Data-Structures/Binary-Search/IterativeBinarySearch.js
@@ -2,12 +2,28 @@ const binarySearch = (arr, target) => {
 
   let left = 0;
   let right = arr.length;
-
-  // finding the middle index:
-  const indexToCheck = Math.floor((left + right) / 2);
-
-  return indexToCheck
-}
+  // while loop that continues to execute
+  // until the left index equals the right index
+  while (right > left) {
+    // find the value of the middle index
+    const indexToCheck = Math.floor((left + right) / 2);
+    const checking = arr[indexToCheck];
+    console.log(indexToCheck);
+      // return the index if the value is equal to the target
+    if (checking === target) {
+      return indexToCheck;
+      // if target is greater htat the value
+      // set left equal to current index plus one
+    } else if (checking < target) {
+      left = indexToCheck + 1;
+    } else {
+      // if target is less than the value
+      // set right equal to the current index
+      right = indexToCheck;
+    }
+  }
+  return null;
+  }
 
 const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
 const target = 28;
