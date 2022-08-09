655bd0c4a312926a019fd343029a1c5883eb6bbc
Neuroplastic1 <shafqat.umair@gmail.com>
2022-08-08 22:14:41 -0400

    Binary search tree: Inserting a Value When inserting a new value into a binary tree, we compare it with the root node’s value:
    If the new value is less than the root node's value
      If a left child node doesn't exist
        Create a new BinaryTree with the new value at a greater depth
        and assign it to the left pointer
      Else
        Recursively call .insert() on the left child node
    Else
      If a right child node doesn't exist
        Create a new BinaryTree with the new value at a greater depth
        and assign it to a right pointer
      Else
        Recursively call .insert() on the right child node
    
        Let’s illustrate the insertion procedure
        with a tree whose root node has the data 100
    
        Insert 50
    50 < 100, left child node doesn't exist, create a left child node
           100
           /
         50
    Insert 125
    125 > 100, right child node doesn't exist, create a right child node
            100
           /   \
          50    125
    Insert 75
    75 < 100, left child node of 50 exists, recursive insert at left child
    75 > 50, right child node doesn't exist, create a right child node
            100
           /   \
          50    125
           \
           75
    Insert 25
    25 < 100, left child node of 50 exists, recursive insert at left child
    25 < 50, left child node doesn't exist, create a left child node
            100
           /   \
          50    125
         /  \
        25  75

--- Non-Linear-Data-Structures/Binary-Search-Trees/BinaryTree.js
+++ Non-Linear-Data-Structures/Binary-Search-Trees/BinaryTree.js
@@ -5,6 +5,29 @@ class BinaryTree {
     this.left= null
     this.right = null
   }
+  // When inserting a new value into a binary tree, compare it with the root node’s value
+  insert(value) {
+    // If the new value is less than the root node's value
+    if (value < this.value) {
+      // If a left child node doesn't exist
+      if (!this.left) {
+        // Create a new BinaryTree with the new value at a greater depth and assign it to the left pointer
+        this.left = new BinaryTree(value, this.depth + 1);
+      } else {
+        // Recursively call .insert() on the left child node
+        this.left.insert(value);
+      }
+    } else {
+      // if a right child node doesn't exist
+      if (!this.right) {
+        // Create a new BinaryTree with the new value at a greater depth and assign it to a right pointer
+        this.right = new BinaryTree(value, this.depth + 1);
+      } else {
+        // Recursively call .insert() on the right child node
+        this.right.insert(value);
+      }
+    }
+  }
 };
 
 module.exports = BinaryTree;
